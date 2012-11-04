b06e2604313fc811a0f4b8b0e7d049f28c89a363
Lee Boynton <lee@lboynton.com>
2012-11-04 18:32:08 +0000

    First version

--- index.php
+++ index.php
@@ -1,1 +1,86 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Lee Boynton</title>
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen, print">
+		<link href="css/site.css" rel="stylesheet" media="screen, print">
+	</head>
+	<body>
+		<div class="container">
+			<h1>Lee Boynton</h1>
+			<small>Homepage: <a href="http://lboynton.com">lboynton.com</a> &middot; GitHub: <a href="https://github.com/lboynton">lboynton</a> &middot; Twitter: <a href="https://twitter.com/leeboynton">leeboynton</a> &middot; LinkedIn: <a href="http://uk.linkedin.com/pub/lee-boynton/24/489/774">Lee Boynton</a> &middot; Delicious: <a href="http://www.delicious.com/l33">l33</a></small>
+			<hr>
+			<p>Currently Web Developer at <a href="http://www.symbiosgroup.co.uk/">Symbios Group</a>, a Hampshire-based company specialising in online dating and social networking.</p>
+			<hr>
+			<h2>Employment</h2>
+			<table class="table">
+				<tbody>
+					<tr>
+						<td class="top left">October 2009 - Present</td>
+						<td class="top">
+							Web Developer at Symbios Group
+							<ul>
+								<li>Lead developer on instant messaging applications, such as web-based multi-user chat room and one-to-one messaging facility. Included making custom modifications to open source software in various languages, such as erlang, python and JavaScript.</li>
+								<li>System administration of RedHat-based servers. Introduced the use of puppet to greatly speed up provisioning and maintaining servers.</li>
+								<li>Advocated and helped develop the use of best software development practices to improve overall code quality, such as test driven development, continuous integration, code review and bug tracking.</li>
+								<li>Promoted the use of git over subversion for new development, helping the development team with any problems.</li>
+								<li>Made improvements to existing websites, such as incorporating a centralised error logging and reporting system.</li>
+							</ul>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<hr>
+			<h2>Education</h2>
+			<table class="table">
+				<tbody>
+					<tr>
+						<td class="top left">October 2008 - September 2009</td>
+						<td class="top middle"><strong>Merit</strong> in MSc Software Engineering from The University of Portsmouth</td>
+						<td class="top">Postgraduate degree involved units on web programming (Java, hibernate), database design, Windows application development (C#), design patterns. Individual project consisted of the development of a Java-based desktop application for visualising wave patterns in the heart, and for simulating conditions such as arrhythmia.</td>
+					</tr>
+					<tr>
+						<td class="left">October 2005 - June 2008</td>
+						<td class="middle"><strong>1<sup>st</sup> Class Honours</strong> in BSc Internet Technology from The University of Portsmouth</td>
+						<td>Undergraduate degree covered topics on a variety of subjects including Java, C and PHP programming, system administration, system security and distributed web systems. Final year project was the development of a desktop instant messaging application with streaming audio features, written in Java and using XMPP.</td>
+					</tr>
+				</tbody>
+			</table>
+			<h2>Skills</h2>
+			<table class="table">
+				<thead>
+					<tr>
+						<th></th>
+						<th></th>
+						<th>Technologies</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td class="top left">Frontend Development</td>
+						<td class="top middle">A number of years experience using jQuery. Up to date with latest trends in frontend development.</td>
+						<td class="top">HTML, CSS, JavaScript, jQuery, backbone.js, require.js, qunit, CoffeeScript, LESS, SASS</td>
+					</tr>
+					<tr>
+						<td class="left">Backend Development</td>
+						<td class="middle">Experienced user of PHP for development </td>
+						<td>PHP, Zend Framework, phpunit, phing, composer, MySQL, Doctrine 2, memcached, redis, MongoDB, sphinx</td>
+					</tr>
+					<tr>
+						<td class="left">System Administration</td>
+						<td class="middle">Comfortable with both RedHat and Debian based systems.</td>
+						<td>CentOS, Ubuntu, puppet, XenServer, apache, nginx, varnish, ejabberd, pfSense, nagios, cacti</td>
+					</tr>
+					<tr>
+						<td class="left">Miscellaneous</td>
+						<td class="middle"></td>
+						<td>git, subversion, XMPP, Jenkins</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+		<script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
