99d48807009f13fd99cd381b7f50ff4e778e68c3
Lee Boynton <lee@lboynton.com>
2013-07-27 22:55:05 +0100

    Add picture of me. Fixes #1.

--- index.php
+++ index.php
@@ -20,88 +20,93 @@
 	</head>
 	<body>
 		<div class="container">
-			<h1>Lee Boynton</h1>
-			<small>
-				<i class="icon-home"></i> Homepage: <a href="http://lboynton.com">lboynton.com</a> &middot; 
-				<i class="icon-github"></i> GitHub: <a href="https://github.com/lboynton">lboynton</a> &middot; 
-				<i class="icon-twitter"></i> Twitter: <a href="https://twitter.com/leeboynton">leeboynton</a> &middot; 
-				<i class="icon-linkedin"></i> LinkedIn: <a href="http://uk.linkedin.com/pub/lee-boynton/24/489/774">Lee Boynton</a> &middot; 
-				<i class="icon-bookmark"></i> Delicious: <a href="http://www.delicious.com/l33">l33</a>
-			</small>
-			<hr>
-			<p>Currently Web Developer at <a href="http://www.symbiosgroup.co.uk/">Symbios Group</a>, a company based in Fareham, UK specialising in online dating and social networking. I also help organise meet ups for <a href="http://www.phphants.co.uk/">PHPHants</a>, a PHP user group for Hampshire, UK.</p>
-			<hr>
-			<h2>Employment</h2>
-			<table class="table">
-				<tbody>
-					<tr>
-						<td class="top left">October 2009 - Present</td>
-						<td class="top">
-							<p><strong>Web Developer at Symbios Group</strong></p>
-							<ul>
-								<li>Lead developer of instant messaging applications using XMPP, such as web-based multi-user chat room and one-to-one messaging facility. Included building JavaScript frontends and making custom modifications to open source software in various languages, such as erlang and python.</li>
-								<li>System administration of RedHat-based servers. Introduced the use of puppet to greatly speed up provisioning and maintaining servers.</li>
-								<li>Advocated and helped develop the use of best software development practices amongst the development team to improve overall code quality, such as test driven development, continuous integration, code review and bug tracking.</li>
-								<li>Promoted the use of git over subversion for new development, helping the development team with any problems.</li>
-								<li>Made improvements to existing websites, such as incorporating a centralised error logging and reporting system.</li>
-							</ul>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<hr>
-			<h2>Education</h2>
-			<table class="table">
-				<tbody>
-					<tr>
-						<td class="top left">October 2008 - September 2009</td>
-						<td class="top middle"><strong>Merit</strong> in MSc Software Engineering from The University of Portsmouth</td>
-						<td class="top">Postgraduate degree involved units on web programming (Java, hibernate), database design, Windows application development (C#), design patterns. Individual project consisted of the development of a Java-based desktop application for visualising wave patterns in the heart, and for simulating conditions such as arrhythmia.</td>
-					</tr>
-					<tr>
-						<td class="left">October 2005 - June 2008</td>
-						<td class="middle"><strong>1<sup>st</sup> Class Honours</strong> in BSc Internet Technology from The University of Portsmouth</td>
-						<td>Undergraduate degree covered topics on a variety of subjects including Java, C and PHP programming, system administration, system security and distributed web systems. Final year project was the development of a desktop instant messaging application with streaming audio features, written in Java and using XMPP.</td>
-					</tr>
-					<tr>
-						<td class="left">September 2000 - July 2003</td>
-						<td class="middle">Sandown High School</td>
-						<td>9 GCSEs including Mathematics grade B, Science grade B and English grade C.</td>
-					</tr>
-				</tbody>
-			</table>
-			<h2>Skills</h2>
-			<table class="table">
-				<thead>
-					<tr>
-						<th></th>
-						<th></th>
-						<th>Technologies</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td class="top left">Frontend Development</td>
-						<td class="top middle">A number of years experience using jQuery. Up to date with latest trends in frontend development.</td>
-						<td class="top">HTML, CSS, JavaScript, jQuery, backbone.js, require.js, qunit, CoffeeScript, LESS, SASS</td>
-					</tr>
-					<tr>
-						<td class="left">Backend Development</td>
-						<td class="middle">10 years of experience with PHP. Familiar with MVC structure for separation of concerns.</td>
-						<td>PHP, Zend Framework (version 1 and 2), phpunit, phing, composer, MySQL, Doctrine 2, memcached, redis, MongoDB, sphinx</td>
-					</tr>
-					<tr>
-						<td class="left">System Administration</td>
-						<td class="middle">Comfortable with both RedHat and Debian based Linux systems.</td>
-						<td>CentOS, Ubuntu, puppet, XenServer, apache, nginx, varnish, ejabberd, pfSense, nagios, cacti</td>
-					</tr>
-					<tr>
-						<td class="left">Miscellaneous</td>
-						<td class="middle"></td>
-						<td>git, subversion, XMPP, Jenkins</td>
-					</tr>
-				</tbody>
-			</table>
+			<div class="header">
+				<img src="img/me.jpeg" alt="Picture of Lee Boynton" class="avatar img-polaroid">
+				<h1>Lee Boynton</h1>
+				<small>
+					<i class="icon-home"></i> Homepage: <a href="http://lboynton.com">lboynton.com</a> &middot; 
+					<i class="icon-github"></i> GitHub: <a href="https://github.com/lboynton">lboynton</a> &middot; 
+					<i class="icon-twitter"></i> Twitter: <a href="https://twitter.com/leeboynton">leeboynton</a> &middot; 
+					<i class="icon-linkedin"></i> LinkedIn: <a href="http://uk.linkedin.com/pub/lee-boynton/24/489/774">Lee Boynton</a> &middot; 
+					<i class="icon-bookmark"></i> Delicious: <a href="http://www.delicious.com/l33">l33</a>
+				</small>
+				<hr>
+				<p>Currently Web Developer at <a href="http://www.symbiosgroup.co.uk/">Symbios Group</a>, a company based in Fareham, UK specialising in online dating and social networking. I also help organise meet ups for <a href="http://www.phphants.co.uk/">PHPHants</a>, a PHP user group for Hampshire, UK.</p>
+			</div>
+			<div class="middle">
+				<hr>
+				<h2>Employment</h2>
+				<table class="table">
+					<tbody>
+						<tr>
+							<td class="top left">October 2009 - Present</td>
+							<td class="top">
+								<p><strong>Web Developer at Symbios Group</strong></p>
+								<ul>
+									<li>Lead developer of instant messaging applications using XMPP, such as web-based multi-user chat room and one-to-one messaging facility. Included building JavaScript frontends and making custom modifications to open source software in various languages, such as erlang and python.</li>
+									<li>System administration of RedHat-based servers. Introduced the use of puppet to greatly speed up provisioning and maintaining servers.</li>
+									<li>Advocated and helped develop the use of best software development practices amongst the development team to improve overall code quality, such as test driven development, continuous integration, code review and bug tracking.</li>
+									<li>Promoted the use of git over subversion for new development, helping the development team with any problems.</li>
+									<li>Made improvements to existing websites, such as incorporating a centralised error logging and reporting system.</li>
+								</ul>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<hr>
+				<h2>Education</h2>
+				<table class="table">
+					<tbody>
+						<tr>
+							<td class="top left">October 2008 - September 2009</td>
+							<td class="top middle"><strong>Merit</strong> in MSc Software Engineering from The University of Portsmouth</td>
+							<td class="top">Postgraduate degree involved units on web programming (Java, hibernate), database design, Windows application development (C#), design patterns. Individual project consisted of the development of a Java-based desktop application for visualising wave patterns in the heart, and for simulating conditions such as arrhythmia.</td>
+						</tr>
+						<tr>
+							<td class="left">October 2005 - June 2008</td>
+							<td class="middle"><strong>1<sup>st</sup> Class Honours</strong> in BSc Internet Technology from The University of Portsmouth</td>
+							<td>Undergraduate degree covered topics on a variety of subjects including Java, C and PHP programming, system administration, system security and distributed web systems. Final year project was the development of a desktop instant messaging application with streaming audio features, written in Java and using XMPP.</td>
+						</tr>
+						<tr>
+							<td class="left">September 2000 - July 2003</td>
+							<td class="middle">Sandown High School</td>
+							<td>9 GCSEs including Mathematics grade B, Science grade B and English grade C.</td>
+						</tr>
+					</tbody>
+				</table>
+				<h2>Skills</h2>
+				<table class="table">
+					<thead>
+						<tr>
+							<th></th>
+							<th></th>
+							<th>Technologies</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td class="top left">Frontend Development</td>
+							<td class="top middle">A number of years experience using jQuery. Up to date with latest trends in frontend development.</td>
+							<td class="top">HTML, CSS, JavaScript, jQuery, backbone.js, require.js, qunit, CoffeeScript, LESS, SASS</td>
+						</tr>
+						<tr>
+							<td class="left">Backend Development</td>
+							<td class="middle">10 years of experience with PHP. Familiar with MVC structure for separation of concerns.</td>
+							<td>PHP, Zend Framework (version 1 and 2), phpunit, phing, composer, MySQL, Doctrine 2, memcached, redis, MongoDB, sphinx</td>
+						</tr>
+						<tr>
+							<td class="left">System Administration</td>
+							<td class="middle">Comfortable with both RedHat and Debian based Linux systems.</td>
+							<td>CentOS, Ubuntu, puppet, XenServer, apache, nginx, varnish, ejabberd, pfSense, nagios, cacti</td>
+						</tr>
+						<tr>
+							<td class="left">Miscellaneous</td>
+							<td class="middle"></td>
+							<td>git, subversion, XMPP, Jenkins</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</div>
 
 		<script src="js/jquery.min.js"></script>
