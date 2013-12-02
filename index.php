d3d0f41a767c51274eb962508fc4ffe1c30ba371
Lee Boynton <lee@lboynton.com>
2013-12-02 23:24:51 +0000

    Set charset

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
 		<title>Lee Boynton</title>
 		<link href="css/bootstrap.css" rel="stylesheet" media="screen, print">
 		<link href="css/font-awesome.css" rel="stylesheet" media="screen, print">
